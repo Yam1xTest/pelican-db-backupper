865eb5a7cc93e382da0a9e1e5f4dbe33deb05d63
Maxim Rychkov <81160491+Yam1x@users.noreply.github.com>
2025-06-27 13:55:23 +0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import boto3
+
+def main():
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.getenv('DESTINATION_DB_AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('DESTINATION_DB_AWS_SECRET_ACCESS_KEY'),
+        endpoint_url=os.getenv('DESTINATION_DB_AWS_ENDPOINT')
+    )
+
+    contents = s3.list_objects_v2(Bucket="pelican-backups")["Contents"]
+
+    print(len(contents))
+
+    if(len(contents) == 1):
+        if(contents[0]['Size'] > 0):
+            exit(1)
+            
+if __name__ == '__main__':
+    main()
