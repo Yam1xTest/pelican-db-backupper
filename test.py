16dde057d3d928f678ba2dcf685598548bb8ac51
Maxim Rychkov <81160491+Yam1x@users.noreply.github.com>
2025-06-30 14:58:28 +0500

    Update test.py

--- test.py
+++ test.py
@@ -10,14 +10,18 @@ def main():
     )
 
     objects_list = s3.list_objects_v2(Bucket="pelican-backups")
-    print(objects_list)
     try:
         contents = objects_list["Contents"]
+        for c in contents:
+            print(c['Size'], c['Key'])
     except:
         raise Exception("Bucket is empty")
 
-    if(not contents[0]['Size'] > 0):
-        raise Exception("File size is 0")
+    if(contents[-1]['Size'] == 0):
+        raise Exception("Backup size is 0")
+
+    if(not round((contents[-1]['Size'] / (2**10)), 1) == 687.1):
+        raise Exception(f"Invalid backup size! Backup size should be 687.1. Current backup size is {round((contents[-1]['Size'] / (2**10)), 1)}")
 
 if __name__ == '__main__':
     main()
